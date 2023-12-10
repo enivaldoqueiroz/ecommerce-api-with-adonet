a15392248ddaa663ea34c5bf3515814a86f9bc2d
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2023-12-10 20:38:20 -0300

    Criação do metodo GetUsuairoById

--- eCommerceApiWithAdoNet/Controllers/UsuariosController.cs
+++ eCommerceApiWithAdoNet/Controllers/UsuariosController.cs
@@ -1,13 +1,55 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using eCommerceApiWithAdoNet.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerceApiWithAdoNet.Controllers
 {
+    /*
+    CRUD
+
+    GET - OBTER A LISTA DE USUARIOS.
+    GET - OBTER O USUARIO PASSANDO O ID.
+    POST - CADASTRAR UM USUARIO.
+    PUT - ATUALIZAR UM USUARIO.
+    DELETE - REMOVER UM USUARIO.
+
+    METHOD HTTP: www.minhaapi.com.br/api/Usuarios
+    www.minhaapi.com.br/api/Usuarios/2
+    */
+
     [Route("api/[controller]")]
     [ApiController]
     public class UsuariosController : ControllerBase
     {
+        private IUsuarioRepository _usuarioRepository;
 
+        public UsuariosController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetUsuarios()
+        {
+            return Ok(_usuarioRepository.GetUsuarios());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUsuairoById(int id)
+        {
+            var usuario = _usuarioRepository.GetUsuarioById(id);
+
+            if (usuario == null)
+                return NotFound();
+
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+
+
+        [HttpPut]
+        [HttpDelete]
     }
 }
 
